8a710b4e3bf2849882e3bb7f198b3cf4d9327fec
Shinikon <123623381+Shinikon@users.noreply.github.com>
2025-05-14 19:29:27 +0500

    Add files via upload

---
+++ Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from '../components/Banner'
+
+ function Main() {
+  return (
+    <div>Main
+      <Banner />
+    </div>
+  )
+}
+
+
+export default Main;
