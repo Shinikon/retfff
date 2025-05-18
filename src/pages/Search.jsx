96352d56a46802dffe3a4ae26dd56101079ab7e3
Shinikon <shinka4u@mail.ru>
2025-05-18 17:37:34 +0500

    Resolved merge conflicts

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import FilmDesc from "../components/FilmDesc";
+
 import search from "../assets/img/search.svg";
 import down from "../assets/img/down.svg";
 
@@ -10,7 +12,6 @@ function Search() {
         <h3>SEARCH...</h3>
 
         <div className="search__bar_sort">
-
           <p>Author</p>
           <div>
             <img src={down} alt="" />
@@ -24,15 +25,37 @@ function Search() {
           <div>
             <img src={down} alt="" />
           </div>
-
         </div>
 
         <div className="search__bar_lupa">
           <img src={search} alt="" />
         </div>
       </div>
+
+      <FilmDesc />
     </div>
   );
 }
 
 export default Search;
+
+/* const [open, setOpen] = React.useState(false); */
+/* 
+<div
+            onClick={() => setOpen(!open)}
+            className={`popup ${open ? "active" : ""}`}
+          >
+            <h3>Select size</h3>
+            <img src={size} alt="Select Size" />
+          </div>
+          {open && (
+            <div className="size-options">
+              <ul>
+                <li>XS</li>
+                <li>S</li>
+                <li>M</li>
+                <li>L</li>
+                <li>XL</li>
+              </ul>
+            </div>
+          )} */
