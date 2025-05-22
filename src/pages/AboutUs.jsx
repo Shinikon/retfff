576ef34c6327fbe2ca37e0e8b5df8b5b5b2e24d8
kamalllova <124335359+kamalllova@users.noreply.github.com>
2025-05-22 12:34:56 +0500

    векрхняя часть страницы о нас

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -8,48 +8,54 @@ function AboutUs() {
   return (
     <div className="about">
       <div className="about__banner">
-        <h4>
+        <h2>
           retflix is ​​more than just a streaming service. it is a community of
           movie and tv show lovers where everyone can find something for
           themselves and share their experiences with others.
-        </h4>
+        </h2>
       </div>
       <div className="about__info">
-        <h3>
-          Retflix is ​​a streaming platform that offers a wide selection of
-          movies, TV series, documentaries and more. We strive to provide our
-          users with an unforgettable viewing experience by offering a variety
-          of content that will satisfy all tastes. On Retflix, you will find
-          both popular new releases and classic works of cinematic art. Our
-          library is constantly updated to ensure that you always have fresh and
-          interesting content to enjoy. We also offer personalized
-          recommendations to help you find exactly what you will love.
-        </h3>
+        <div className="about__info_text">
+          <h3>
+            Retflix is ​​a streaming platform that offers a wide selection of
+            movies, TV series, documentaries and more. We strive to provide our
+            users with an unforgettable viewing experience by offering a variety
+            of content that will satisfy all tastes. On Retflix, you will find
+            both popular new releases and classic works of cinematic art. Our
+            library is constantly updated to ensure that you always have fresh
+            and interesting content to enjoy. We also offer personalized
+            recommendations to help you find exactly what you will love.
+          </h3>
 
-        <ul>
-          What you'll find on Retflix:
-          <li>
-            A huge library of content: From Hollywood blockbusters to gripping
-            series, documentaries to anime, we have something for everyone.
-          </li>
-          <li>
-            Personalized recommendations: Our intelligent recommendation engine
-            analyzes your preferences and suggests content we know you'll love.
-          </li>
-          <li>
-            User-friendly interface: Enjoy watching on any device, from TV to
-            smartphone, with our intuitive and easy-to-use interface.
-          </li>
-          <li>
-            Retflix original content: Discover exclusive Retflix-produced movies
-            and series that you won't find anywhere else.
-          </li>
-          <li>
-            Watch anywhere, anytime: Download content for offline viewing and
-            enjoy your favorite movies and series even without an internet
-            connection.
-          </li>
-        </ul>
+          <ul>
+            What you'll find on Retflix:
+            <li>
+              <b>A huge library of content:</b> From Hollywood blockbusters to
+              gripping series, documentaries to anime, we have something for
+              everyone.
+            </li>
+            <li>
+              <b>Personalized recommendations:</b> Our intelligent
+              recommendation engine analyzes your preferences and suggests
+              content we know you'll love.
+            </li>
+            <li>
+              <b> User-friendly interface:</b> Enjoy watching on any device,
+              from TV to smartphone, with our intuitive and easy-to-use
+              interface.
+            </li>
+            <li>
+              <b> Retflix original content:</b> Discover exclusive
+              Retflix-produced movies and series that you won't find anywhere
+              else.
+            </li>
+            <li>
+              <b> Watch anywhere, anytime:</b> Download content for offline
+              viewing and enjoy your favorite movies and series even without an
+              internet connection.
+            </li>
+          </ul>
+        </div>
 
         <img src={aboutUs} alt="" />
       </div>
